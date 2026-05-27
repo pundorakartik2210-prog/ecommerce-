385a078aa093ca81de99709d106b8087c13bcfa2
Shivam-sharma23 <shivam.sh0023@gmail.com>
2026-05-27 17:13:22 +0530

    config: fallback default DB connection to mysql if DB_HOST is set

--- backend/config/database.php
+++ backend/config/database.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', env('DB_HOST') ? 'mysql' : 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
